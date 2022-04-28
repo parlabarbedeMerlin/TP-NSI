4624cfb5378626d56353f033b181dfa059a89c92
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2022-04-28 14:09:50 +0200

    TP20

--- sujetstp/22_NSI_20/22-NSI-20.py
+++ sujetstp/22_NSI_20/22-NSI-20.py
@@ -1,41 +1,52 @@
+def xor(tab1, tab2):
+    assert len(tab1) == len(tab2), "les deux tableaux ne font pas la meme taille"
+    resultat = []
+    taille = len(tab1)
+    for compteur in range(taille):
+        resultat.append(tab1[compteur]^tab2[compteur])
+    return resultat
+
 class Carre:
     def __init__(self, tableau = [[]]):
         self.ordre = len(tableau)
         self.valeurs = tableau
-    
+
     def affiche(self):
         '''Affiche un carré'''
         for i in range(self.ordre):
             print(self.valeurs[i])
-    
+
     def somme_ligne(self, i):
         '''Calcule la somme des valeurs de la ligne i'''
         return sum(self.valeurs[i])
-    
+
     def somme_col(self, j):
-        '''calcule la somme des valeurs de la colonne j'''
+        '''Calcule la somme des valeurs de la colonne j'''
         return sum([self.valeurs[i][j] for i in range(self.ordre)])
 
 def est_magique(carre):
     n = carre.ordre
     s = carre.somme_ligne(0)
-        
-    #test de la somme de chaque ligne
-    for i in range(..., ...):
+    for i in range(1, n):
         if carre.somme_ligne(i) != s:
-            return ...
-        
-    #test de la somme de chaque colonne
+            return False
     for j in range(n):
-        if ... != s:
+        if carre.somme_ligne(j) != s:
             return False
-         
-    #test de la somme de chaque diagonale
-    if sum([carre.valeurs[...][...] for k in range(n)]) != s:
-            return False
+    if sum([carre.valeurs[k][k] for k in range(n)]) != s:
+        return False
     if sum([carre.valeurs[k][n-1-k] for k in range(n)]) != s:
-            return False
-    
-    return ...   
+        return False
+    return s
 
+c1 = Carre([[1, 1],
+            [1, 1]])
 
+c2 = Carre([[2, 9, 4],
+            [7, 5, 3],
+            [6, 1, 8]])
+
+c3 = Carre([[4, 5, 16, 9],
+            [14, 7, 2, 11],
+            [3, 10, 15, 6],
+            [13, 12, 8, 1]])
