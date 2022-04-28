aeb8f802537b1b3c543bf9766a6401271decf8c9
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2022-04-28 14:02:08 +0200

    TP18

--- sujetstp/22_NSI_18/22-NSI-18.py
+++ sujetstp/22_NSI_18/22-NSI-18.py
@@ -1,13 +1,25 @@
+t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7]
+annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
+
+def mini(releve,date):
+    min_id=0
+    for i in range(len(releve)):
+        min_id = i if releve[i]<=releve[min_id] else min_id
+    return releve[min_id],date[min_id]
+
+print( mini(t_moy, annees))
+
+
 def inverse_chaine(chaine):
-    result = ...
+    result = ''
     for caractere in chaine:
-       result = ...
+        result = caractere + result
     return result
 
 def est_palindrome(chaine):
     inverse = inverse_chaine(chaine)
-    return ...
-    
+    return chaine == inverse
+
 def est_nbre_palindrome(nbre):
-    chaine = ...
+    chaine = str(nbre)
     return est_palindrome(chaine)
