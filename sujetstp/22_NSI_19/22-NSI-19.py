b60a7a38b2741e623a49b638d6ddd03c2e025d5f
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2022-04-28 14:06:54 +0200

    TP19

--- sujetstp/22_NSI_19/22-NSI-19.py
+++ sujetstp/22_NSI_19/22-NSI-19.py
@@ -1,13 +1,22 @@
-def chercher(T,n,i,j):
-    if i < 0 or ??? :
-        print("Erreur")
-        return None    
+def multiplication(a,b):
+    r=0
+    for i in range(a):
+        r+=b
+    return r
+
+print(multiplication(3,2))
+
+
+def chercher(T, n, i, j):
+    if i < 0 or j >= len(T) :
+        print('Erreur')
+        return None
     if i > j :
         return None
-    m = (i+j) // ???
-    if T[m] < ??? :
-        return chercher(T, n, ??? , ???)
-    elif ??? :
-        return chercher(T, n, ??? , ??? )
+    m = (i + j) // 2
+    if T[m] < n :
+        return chercher(T, n, m + 1, j)
+    elif T[m] > n :
+        return chercher(T, n, i, m - 1 )
     else :
-        return ??? 
+        return m
